54e90027c28225c0963b669a1eba49fbaa455621
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-02-14 11:28:31 -0300

    feat: Adicionando funções para fazer o update do usuário e modificando a função de criação de usuários

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -14,13 +14,24 @@ async function createNewUser(req, res){
     const { name, email } = req.body
     try{
         await client.connect()
-        console.log('Conectado ao Mongodb!')
-        const mongodb = client.db(`${process.env.MONGO_DATABASE}`).collection('Usuarios')
-            await mongodb.insertOne({
-                name: name,
-                email: email
+            console.log('Conectado ao Mongodb!')
+        let countUsers = 0
+        let users = await returnUsers()
+            users.forEach(item=>{
+                if(item.email == email){
+                    count = 1
+                }
             })
-        res.status(200).send('inserido')
+        if(countUsers == 0){
+            const mongodb = client.db(`${process.env.MONGO_DATABASE}`).collection('Usuarios')
+                await mongodb.insertOne({
+                    name: name,
+                    email: email
+                })
+            res.status(200).send('inserido')
+        }else{
+            res.status(400).send('Email já registrado!')
+        }
     }catch(error){
         console.log(error)
     }finally{
@@ -49,7 +60,48 @@ async function getUsers(req, res){
 
 }
 
+async function returnUsers(){
+    try {
+        const mongodb = client.db(`${process.env.MONGO_DATABASE}`).collection('Usuarios')
+        let results = []
+        await mongodb.find().forEach(item=>{results.push(item)})
+
+        return results
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function updateUser(req, res){
+    const { newName, oldEmail, newEmail } = req.body
+    try {
+        await client.connect()
+        const mongodb = client.db(`${process.env.MONGO_DATABASE}`).collection('Usuarios')
+            console.log('Conectado ao Mongo!')
+        let users = await returnUsers()
+        let countUsers = 0
+        users.forEach(item=>{
+            if(item.email == oldEmail){
+                countUsers = 1
+            }
+        })
+        if(countUsers == 1){
+            const query = {email: oldEmail}
+            const update = {$set: {name:newName, email: newEmail}}
+                mongodb.updateOne(query,update)
+                .then(()=>console.log('Atualizao com sucesso'))
+                .finally(()=>client.close)
+               return  res.status(200).send('atualizado')
+        }else{
+            return  res.status(400).send('Não existe esse email para ser atualizado!')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
     createNewUser,
-    getUsers
+    getUsers,
+    updateUser
 }
