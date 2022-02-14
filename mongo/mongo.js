f6c132ad8c0c99c5f4b13b9bd87a522459b0cb7f
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-02-14 10:35:56 -0300

    feat: Criando funções para criar e recuperar usuários

--- mongo/mongo.js
+++ mongo/mongo.js
@@ -1,9 +1,56 @@
+//Importando arquivos de configuração
 require('dotenv').config()
 
+//importando classe do mongodb
 const { MongoClient } = require('mongodb')
 
-const client = new MongoClient(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`)
+//Setando configuração da classe para fazer a conexão
+const client = new MongoClient(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,{
+    useUnifiedTopology: true
+})
 
-client.connect().then(()=>{
-    console.log('Conectado ao mongo')
-}).catch(error=>console.log(error))
+//Criando função para fazer conexão com o banco e adicionar novas postagens
+async function createNewPost(req, res){
+    const { id, text, title } = req.body
+    try{
+        await client.connect()
+        console.log('Conectado ao Mongodb!')
+        const mongodb = client.db(`${process.env.MONGO_DATABASE}`).collection('Usuarios')
+            await mongodb.insertOne({
+                id: id,
+                text: text,
+                title: title
+            })
+        res.status(200).send('inserido')
+    }catch(error){
+        console.log(error)
+    }finally{
+        await client.close()
+        console.log('Desconectado!')
+    }
+}
+
+//Criando função para recuperar os dados salvos no banco 
+async function getPosts(req, res){
+    try{
+        await client.connect()
+        console.log('Conectado ao Mongodb!')
+        const mongodb = client.db(`${process.env.MONGO_DATABASE}`).collection('Usuarios')
+        let results = []
+        await mongodb.find().forEach(item=>{results.push(item)})
+
+        return res.status(200).send(results)
+        
+    }catch(error){
+        console.log(error)
+    }finally{
+        await client.close()
+        console.log('Desconectado!')
+    }
+
+}
+
+module.exports = {
+    createNewPost,
+    getPosts
+}
